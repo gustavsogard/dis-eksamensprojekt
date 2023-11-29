d9c7b3d8440b0734bdc83e2397c9826650a7f1f0
gubstav <gustav.christian@hotmail.com>
2023-11-29 13:27:10 +0100

    Refactoring with post endpoint

--- src/server.js
+++ src/server.js
@@ -36,7 +36,8 @@ const orders = [
       { id: 2, name: "Juice" },
     ],
     store_name: "Copenhagen",
-    phoneNum: "+4520381866"
+    phoneNum: "+4520381866",
+    external_api_key: "none",
   },
   {
     id: uuidv4(),
@@ -44,7 +45,19 @@ const orders = [
     customer: "Donald",
     products: [{ id: 1, name: "Avocado Sandwich" }],
     store_name: "Copenhagen",
-    phoneNum: "+4520389422"
+    phoneNum: "+4520389422",
+    external_api_key: "none",
+  },
+];
+
+const external_providers = [
+  {
+    api_key: "1234",
+    external_name: "UberEats",
+  },
+  {
+    api_key: "5678",
+    external_name: "JustEat",
   },
 ];
 
@@ -64,7 +77,10 @@ db.serialize(function () {
     CREATE TABLE IF NOT EXISTS stores (id TEXT PRIMARY KEY, store_name TEXT NOT NULL UNIQUE, password TEXT NOT NULL);
   `);
   db.run(`
-    CREATE TABLE IF NOT EXISTS orders (id TEXT PRIMARY KEY, status TEXT NOT NULL, customer TEXT NOT NULL, products TEXT NOT NULL, store_name TEXT NOT NULL, phoneNum TEXT NOT NULL);
+    CREATE TABLE IF NOT EXISTS orders (id TEXT PRIMARY KEY, status TEXT NOT NULL, customer TEXT NOT NULL, products TEXT NOT NULL, store_name TEXT NOT NULL, phoneNum TEXT NOT NULL, external_api_key TEXT NOT NULL);
+  `);
+  db.run(`
+    CREATE TABLE IF NOT EXISTS api_keys (api_key TEXT PRIMARY KEY, external_name TEXT NOT NULL);
   `);
 
   // laver 10 salt rounds
@@ -93,6 +109,27 @@ db.serialize(function () {
     });
   })();
   (async () => {
+    external_providers.forEach(async (provider) => {
+      db.get(
+        `SELECT * FROM api_keys WHERE external_name = ?`,
+        [provider.external_name],
+        (err, row) => {
+          if (err) {
+            console.error(err.message);
+          }
+          if (row) {
+            return;
+          } else {
+            db.run(`INSERT INTO api_keys VALUES (?, ?)`, [
+              provider.api_key,
+              provider.external_name,
+            ]);
+          }
+        }
+      );
+    });
+  })();
+  (async () => {
     // Check if orders already exist
     db.get("SELECT COUNT(*) as count FROM orders", (err, row) => {
       if (err) {
@@ -102,21 +139,20 @@ db.serialize(function () {
         return;
       } else {
         orders.forEach(async (order) => {
-          db.run("INSERT INTO orders VALUES (?, ?, ?, ?, ?, ?)", [
+          db.run("INSERT INTO orders VALUES (?, ?, ?, ?, ?, ?, ?)", [
             order.id,
             order.status,
             order.customer,
             JSON.stringify(order.products),
             order.store_name,
-            encryptNum(order.phoneNum)
+            encryptNum(order.phoneNum),
+            order.external_api_key,
           ]);
         });
       }
     });
   })();
 });
-
-
 
 app
   .route("/login")
@@ -135,13 +171,17 @@ app
   .get((req, res) => {
     const token = req.cookies.JWT;
     console.log("token", token);
-    const store_name = jwt.verify(token, process.env.secret_key, (err, decoded) => {
-      if (err) {
-        return res.status(403).json({ message: "Invalid token" });
-      } else {
-        return decoded.locationName;
+    const store_name = jwt.verify(
+      token,
+      process.env.secret_key,
+      (err, decoded) => {
+        if (err) {
+          return res.status(403).json({ message: "Invalid token" });
+        } else {
+          return decoded.locationName;
+        }
       }
-    });
+    );
     console.log(store_name);
 
     db.all(
@@ -161,21 +201,62 @@ app
       }
     );
   })
-  .post((req, res) => {
-    const order = req.body;
-    order.id = uuidv4();
-    order.status = "created";
-    db.run("INSERT INTO orders VALUES (?, ?, ?, ?, ?, ?)", [
-      order.id,
-      order.status,
-      order.customer,
-      JSON.stringify(order.products),
-      order.store_name,
-      encryptNum(order.phoneNum)
-    ]);
-    sendSMS(encryptNum(order.phoneNum, 'created'));
-    io.emit("newOrder", order);
-    res.json(order);
+  .post(async (req, res) => {
+    try {
+      const jwtToken = req.cookies.JWT;
+      const bearerToken = req.headers["authorization"]?.split(" ")[1];
+      let store_name = undefined;
+
+      if (!jwtToken && !bearerToken) {
+        return res.status(401).json({ message: "No jwt or bearer token" });
+      }
+
+      if (jwtToken) {
+        const decoded = await new Promise((resolve, reject) => {
+          jwt.verify(jwtToken, process.env.secret_key, (err, decoded) => {
+            if (err) reject("Invalid token");
+            resolve(decoded);
+          });
+        });
+        store_name = decoded.locationName;
+      }
+
+      if (bearerToken) {
+        const row = await new Promise((resolve, reject) => {
+          db.get(
+            `SELECT * FROM api_keys WHERE api_key = ?`,
+            [bearerToken],
+            (err, row) => {
+              if (err) reject("Server error");
+              if (!row) reject("Invalid token");
+              resolve(row);
+            }
+          );
+        });
+      }
+
+      const order = req.body;
+      order.id = uuidv4();
+      order.status = "created";
+      db.run("INSERT INTO orders VALUES (?, ?, ?, ?, ?, ?, ?)", [
+        order.id,
+        order.status,
+        order.customer,
+        JSON.stringify(order.products),
+        store_name ?? order.store_name,
+        encryptNum(order.phoneNum),
+        bearerToken ?? "none",
+      ]);
+      sendSMS(order.phoneNum, "created", false);
+      io.emit("newOrder", order);
+      res.json(order);
+    } catch (error) {
+      if (error === "Invalid token") {
+        return res.status(403).json({ message: error });
+      } else {
+        return res.status(500).json({ message: "An error occurred" });
+      }
+    }
   });
 
 // verifyPassword() for copenhagen function
@@ -259,8 +340,6 @@ io.on("connection", (socket) => {
     });
   });
 });
-
-
 
 app.use("/public", express.static(path.join(__dirname, "../client/public")));
 
--- src/sms.js
+++ src/sms.js
@@ -7,14 +7,15 @@ const messages = {
     created: 'Hey det er JOE. Din ordre er blevet modtaget, du vil få en besked når den er igangsat.',
     accepted: 'Hey det er JOE. Din ordre er igangsat, du vil få en besked når den er klar.',
     done: 'Hey det er JOE. Din ordre er klar til afhentning.',
+    rejected: 'Hey det er JOE. Din ordre er blevet afvist, kontakt os venligst for mere information.',
 }
 
-const sendSMS = (phoneNum, status) => {
+const sendSMS = (phoneNum, status, encryptFlag = true) => {
     client.messages
         .create({
             body: messages[status],
             from: '+12052364320',
-            to: decryptNum(phoneNum)
+            to: encryptFlag ? decryptNum(phoneNum) : phoneNum
         })
 }
 
